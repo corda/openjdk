26c11ce42225581ba68f243cac30309ff909d9f0
Chris Rankin <chris.rankin@r3.com>
2017-06-13 14:20:48 +0100

    Remove saving/serialising Properties.

--- jdk/src/share/classes/sun/misc/VMSupport.java
+++ jdk/src/share/classes/sun/misc/VMSupport.java
@@ -24,7 +24,6 @@
  */
 package sun.misc;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.Set;
@@ -49,35 +48,6 @@ public class VMSupport {
         return agentProps;
     }
     private static native Properties initAgentProperties(Properties props);
-
-    /**
-     * Write the given properties list to a byte array and return it. Properties with
-     * a key or value that is not a String is filtered out. The stream written to the byte
-     * array is ISO 8859-1 encoded.
-     */
-    private static byte[] serializePropertiesToByteArray(Properties p) throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream(4096);
-
-        Properties props = new Properties();
-
-        // stringPropertyNames() returns a snapshot of the property keys
-        Set<String> keyset = p.stringPropertyNames();
-        for (String key : keyset) {
-            String value = p.getProperty(key);
-            props.put(key, value);
-        }
-
-        props.store(out, null);
-        return out.toByteArray();
-    }
-
-    public static byte[] serializePropertiesToByteArray() throws IOException {
-        return serializePropertiesToByteArray(System.getProperties());
-    }
-
-    public static byte[] serializeAgentPropertiesToByteArray() throws IOException {
-        return serializePropertiesToByteArray(getAgentProperties());
-    }
 
     /*
      * Returns true if the given JAR file has the Class-Path attribute in the
